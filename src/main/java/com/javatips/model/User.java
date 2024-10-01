ad1c2bdce9d24d2342b70e29e5924df1140a4efb
Kay Tang <126426690+ktan185@users.noreply.github.com>
2024-10-01 21:11:36 +1300

    feat: changed email to be id.
    - Reduce redundant info stored in db.

--- src/main/java/com/javatips/model/User.java
+++ src/main/java/com/javatips/model/User.java
@@ -5,21 +5,11 @@ import org.springframework.data.annotation.Id;
 public class User {
 
     @Id
-    private String id;
+    private String email;
 
     private String firstName;
 
     private String lastName;
-
-    private String email;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
