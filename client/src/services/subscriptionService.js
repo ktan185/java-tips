ecf2421c1a421464acfaadd1f8519e57da6e69a8
Kay Tang <126426690+ktan185@users.noreply.github.com>
2024-10-05 11:05:29 +1300

    feat: added proxying for frontend

--- client/src/services/subscriptionService.js
+++ client/src/services/subscriptionService.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-// const API_URL = process.env.REACT_APP_API_URL
-const API_URL = 'http://localhost:8080'
 export const subscribe = async (user) => {
-    return await axios.post(`${API_URL}/users/signup`, user)
+    return await axios.post(`/users/signup`, user)
 }
